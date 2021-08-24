8e63fe740fb3e4770ecbcd68227f616220141917
Admin <Admin@DESKTOP-VUBC6JP>
2021-08-24 21:50:52 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿/// Homework No. 1 Exercise No. 2
+/// File Name: HomeworkPart2
+/// @author: Nasi Bode
+/// Date: August 24, 2021
+/// 
+/// Problem Statement: Write program that displays a simple picture of your choice. 
+/// Hint: Write a sequence of WriteLine statements that display lines of asteriks or other
+/// characters and blanks. PrintScreen your results
+///
+/// Overall Plan: 
+/// 1. print message to the screen explaining what the program does
+/// 2. display picture to the screen
+/// 3. close the program
+/// 
+
+
+using System;
+
+namespace HomeworkPart2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Print Message to Screen Explaining Program
+            Console.WriteLine("This Program Shows a Flower!\n");
+            //Start Sequence of WriteLines to create Picture
+            Console.WriteLine("____________$$$$$$");
+            Console.WriteLine("___________$$$$$$$$");
+            Console.WriteLine("___$$$$$$$__$$$$$$__$$$$$$");
+            Console.WriteLine("__$$$$$$$$$_ $$$$$_ $$$$$$$$");
+            Console.WriteLine("___$$$$$$$$$_$$$$$_$$$$$$$$");
+            Console.WriteLine("____$$$$$$$$_ $$$$_$$$$$$$");
+            Console.WriteLine("________$$$$$_$$$_$$$$$");
+            Console.WriteLine("_$$$$$$$$$_ $____$_$$$$$$$$$");
+            Console.WriteLine("$$$$$$$$$$ $______$ $$$$$$$$$$");
+            Console.WriteLine("$$$$$$$$$$$______ $$$$$$$$$$$");
+            Console.WriteLine("_$$$$$$$$$_$_____$_$$$$$$$$$");
+            Console.WriteLine("_________$$$$$_$$$_$$$$$");
+            Console.WriteLine("_____$$$$$$$$_ $$$$_$$$$$$$");
+            Console.WriteLine("____$$$$$$$$$_$$$$$_$$$$$$$$");
+            Console.WriteLine("___$$$$$$$$$_ $$$$$_ $$$$$$$$");
+            Console.WriteLine("____$$$$$$$__$$$$$$__$$$$$$");
+            Console.WriteLine("____________$$$$$$$$_$$");
+            Console.WriteLine("_____________$$$$$$_ $$");
+            Console.WriteLine("______$$$$$_________$$");
+            Console.WriteLine("_____$$$$$$$_______ $$");
+            Console.WriteLine("___$$$$$$$$$$$_____$$");
+            Console.WriteLine("_____ $$$$$$$$$___ $$");
+            Console.WriteLine("________$$$$$$$__$$");
+            Console.WriteLine("__________$$$$$_$$");
+            Console.WriteLine("___________$$$$$$");
+            Console.WriteLine("____________$$$$");
+            Console.WriteLine("_____________$$");
+            Console.WriteLine("____________$$____$$$$$$$");
+            Console.WriteLine("___________ $$___$$$$$$$$$$");
+            Console.WriteLine("___________$$__$$$$$$$$");
+            Console.WriteLine("___________$$_$$$$$$");
+            Console.WriteLine("___________$$_$$$$$");
+            Console.WriteLine("___________$$$$$$");
+            Console.WriteLine("___________$$$$");
+            Console.WriteLine("___________$$");
+
+        }
+    }
+}
